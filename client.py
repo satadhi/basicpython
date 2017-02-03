c6d486a8457a24ae8fc09ae7fe916101daabdb57
satadhi <haldersatadhi@gmail.com>
2017-02-04 00:44:38 +0530

    3.feb

--- client.py
+++ client.py
@@ -2,5 +2,6 @@ import socket
 host = '127.0.0.2'
 port =  5555
 s = socket.socket()
-s.bind((host,port))
-s.sendall("hi man its working !".encode())
+s.connect((host,port))
+s = "hi man its working !"
+s.sendall(str.encode(s))
