6398372de9d208d6e1ea9c70458945cb25a59f6f
satadhi <haldersatadhi@gmail.com>
2017-02-03 23:57:43 +0530

    adding servr and clernt

---
+++ server.py
@@ -0,0 +1,13 @@
+import socket
+host = '127.0.0.1'
+port =  5555
+s = socket.socket()
+s.bind((host,port))
+s.listen(1)
+conn,rport=s.accept()
+print("connection from :",rport)
+while True:
+    data = conn.recv(1024)
+    if not data:
+        print(data.decode('utf-8'))
+        break
